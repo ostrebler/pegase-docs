862a30962e0b0dccafee6388ebf630d29aaf77ac
ostrebler <strebler.olivier@hotmail.fr>
2021-08-05 03:58:45 +0200

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <>test</>;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
